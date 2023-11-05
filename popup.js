d161931dca6c951e336aecf5ad792340b7d75fa9
luke.piotrak <48261254+luk-pio@users.noreply.github.com>
2023-11-05 05:04:44 +0000

    add loader

--- popup.js
+++ popup.js
@@ -1,39 +1,46 @@
-document.getElementById('find-button').addEventListener('click', () => {
+// Simulate a network request
+async function getAnalysis(url) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(response.articles);
+    }, 10000); // Wait for 10 seconds
+  });
+  // Use fetch to get a a response from the server with the url as the query parameter
+  // const encodedUrl = btoa(url)
+  // const response = await fetch(`http://localhost:5000/analyze?url=${encodedUrl}`)
+  // const resp = response.json()
+  // return resp.articles
+}
+
+document.getElementById('find-button').addEventListener('click', async () => {
+  const findButton = document.getElementById('find-button');
   const tableContainer = document.getElementById('table-container');
-  tableContainer.appendChild(createTableWithHeader());
-  const tableBody = document.querySelector('tbody');
-  const articles = response.articles;
+  tableContainer.innerHTML = '';
+
+  // Replace the button with a loader
+  const loader = createLoader();
+  findButton.replaceWith(loader);
+
+  const articles = await getAnalysis(document.URL);
+
+  // Replace the loader back with the button
+  loader.replaceWith(findButton);
+
   for (let article of articles) {
     const row = createTableRow(article);
-    tableBody.appendChild(row);
+    tableContainer.appendChild(row);
   }
 });
 
-function createTableWithHeader() {
-  // Create table
-  const table = document.createElement('table');
-
-  // Create table header
-  const thead = document.createElement('thead');
-  const headerRow = document.createElement('tr');
-  const headers = ['Title', 'Bias', 'Topic', 'Key Points'];
-
-  headers.forEach(headerText => {
-    const th = document.createElement('th');
-    th.textContent = headerText;
-    headerRow.appendChild(th);
-  });
-
-  thead.appendChild(headerRow);
-  table.appendChild(thead);
-
-  // Create table body
-  const tbody = document.createElement('tbody');
-  table.appendChild(tbody);
-
-  return table;
+function createLoader() {
+    const loader = document.createElement('div');
+    loader.classList.add('lds-ripple');
+    const div1 = document.createElement('div');
+    const div2 = document.createElement('div');
+    loader.appendChild(div1);
+    loader.appendChild(div2);
+    return loader;
 }
-
 
 function createTableCell(tag, innerHTML) {
   const element = document.createElement(tag);
@@ -51,40 +58,35 @@ function createKeyPointsList(keyPoints) {
 }
 
 function createTableRow(article) {
-  const row = document.createElement('tr');
-  const title = createTableCell('td', article.title);
-  const bias = createTableCell('td', article.bias);
-  const topic = createTableCell('td', article.topic);
-  const keyPoints = createTableCell('td', '');
+  const row = document.createElement('div');
+
+  const title = createTableCell('div', article.title);
+  title.classList.add('article-title');
+
+  const tagsContainer = document.createElement('div');
+  tagsContainer.classList.add('tags-container');
+
+  const domain = createTableCell('div', `<b>${(new URL(article.url).hostname).replace('www.', '')}</b>`);
+  const bias = createTableCell('div', `Bias: ${article.bias}`);
+  const topic = createTableCell('div', article.topic);
+  tagsContainer.appendChild(domain);
+  tagsContainer.appendChild(bias);
+  tagsContainer.appendChild(topic);
+
+  const keyPoints = createTableCell('div', '');
   keyPoints.appendChild(createKeyPointsList(article.key_points));
 
   row.appendChild(title);
-  row.appendChild(bias);
-  row.appendChild(topic);
+  row.appendChild(tagsContainer);
   row.appendChild(keyPoints);
 
   return row;
 }
 
-function tableComponent() {
-  return {
-      items: [],
-      populateTable() {
-          // This should be replaced with an actual data fetching from an API
-          this.items = [
-              { id: 1, name: 'John Doe', email: 'john@example.com' },
-              { id: 2, name: 'Jane Doe', email: 'jane@example.com' },
-              { id: 3, name: 'Terry Adams', email: 'terry@example.net' },
-              { id: 4, name: 'Sandy Cheeks', email: 'sandy@example.org' }
-          ];
-      }
-  }
-}
-
 const response = {
   "articles": [
-    { "key_points": [" a Canadian astronomer named Robert Weryk was reviewing images captured by a telescope known as Pan-starrs1 when he noticed something strange.", "Weryk alerted colleagues, who began tracking the dot from other observatories. The more they looked, the more puzzling its behavior seemed. The object was small, with an area roughly that of a city block. As it tumbled through space, its brightness varied so much\u2014by a factor of ten\u2014that it had to have a very odd shape."], "bias": 0.9, "title": "Anal or nothing, says the sex committee", "topic": "Anal is the supreme choice" },
-    { "key_points": [" a Canadian astronomer named Robert Weryk was reviewing images captured by a telescope known as Pan-starrs1 when he noticed something strange.", "Weryk alerted colleagues, who began tracking the dot from other observatories. The more they looked, the more puzzling its behavior seemed. The object was small, with an area roughly that of a city block. As it tumbled through space, its brightness varied so much\u2014by a factor of ten\u2014that it had to have a very odd shape."], "bias": 0.9, "title": "Anal or nothing, says the sex committee", "topic": "Anal is the supreme choice" },
-    { "key_points": [" a Canadian astronomer named Robert Weryk was reviewing images captured by a telescope known as Pan-starrs1 when he noticed something strange.", "Weryk alerted colleagues, who began tracking the dot from other observatories. The more they looked, the more puzzling its behavior seemed. The object was small, with an area roughly that of a city block. As it tumbled through space, its brightness varied so much\u2014by a factor of ten\u2014that it had to have a very odd shape."], "bias": 0.9, "title": "Anal or nothing, says the sex committee", "topic": "Anal is the supreme choice" },
+    { "key_points": [" a Canadian astronomer named Robert Weryk was reviewing images captured by a telescope known as Pan-starrs1 when he noticed something strange.", "Weryk alerted colleagues, who began tracking the dot from other observatories. The more they looked, the more puzzling its behavior seemed. The object was small, with an area roughly that of a city block. As it tumbled through space, its brightness varied so much\u2014by a factor of ten\u2014that it had to have a very odd shape."], "bias": 0.9, "title": "Anal or nothing, says the sex committee", "topic": "Anal is the supreme choice", "url": "https://www.theguardian.com/world/live/2023/nov/04/israel-hamas-war-live-blinken-set-to-meet-with-middle-east-foreign-ministers-in-jordan-airstrike-on-gaza-ambulance-kills-15" },
+    { "key_points": [" a Canadian astronomer named Robert Weryk was reviewing images captured by a telescope known as Pan-starrs1 when he noticed something strange.", "Weryk alerted colleagues, who began tracking the dot from other observatories. The more they looked, the more puzzling its behavior seemed. The object was small, with an area roughly that of a city block. As it tumbled through space, its brightness varied so much\u2014by a factor of ten\u2014that it had to have a very odd shape."], "bias": 0.9, "title": "Anal or nothing, says the sex committee", "topic": "Anal is the supreme choice", "url": "https://www.theguardian.com/world/live/2023/nov/04/israel-hamas-war-live-blinken-set-to-meet-with-middle-east-foreign-ministers-in-jordan-airstrike-on-gaza-ambulance-kills-15" },
+    { "key_points": [" a Canadian astronomer named Robert Weryk was reviewing images captured by a telescope known as Pan-starrs1 when he noticed something strange.", "Weryk alerted colleagues, who began tracking the dot from other observatories. The more they looked, the more puzzling its behavior seemed. The object was small, with an area roughly that of a city block. As it tumbled through space, its brightness varied so much\u2014by a factor of ten\u2014that it had to have a very odd shape."], "bias": 0.9, "title": "Anal or nothing, says the sex committee", "topic": "Anal is the supreme choice", "url": "https://www.theguardian.com/world/live/2023/nov/04/israel-hamas-war-live-blinken-set-to-meet-with-middle-east-foreign-ministers-in-jordan-airstrike-on-gaza-ambulance-kills-15" },
   ]
 } 
